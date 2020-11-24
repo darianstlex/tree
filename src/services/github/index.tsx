0127eb2aa5e71de73ed74b1b63e4c51a75fc93c3
darianstlex <iv.uzun@gmail.com>
2020-11-24 02:16:41 +0000

    exclude types from export

--- src/services/github/index.tsx
+++ src/services/github/index.tsx
@@ -1,11 +1,9 @@
 import { gitSearch } from './effects';
 import { reset } from './events';
 import { $list as $ } from './store';
-import * as types from './types';
 
 export const githubService = {
   $,
   gitSearch,
   reset,
-  types,
 }
